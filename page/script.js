8232a1c35b55189d2241a3877c23779b84d547f6
dunkelreiss <muhammadarif0116@gmail.com>
2021-12-08 16:45:29 +0700

    first commit

---
+++ page/script.js
@@ -0,0 +1,241 @@
+window.addEventListener('scroll', awal);
+
+function awal() {
+    var reveals = document.querySelectorAll('#awal1');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', gambar);
+
+function gambar() {
+    var reveals = document.querySelectorAll('.gambar');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+
+window.addEventListener('scroll', container2);
+
+function container2() {
+    var reveals = document.querySelectorAll('.container2 .card');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', container3);
+
+function container3() {
+    var reveals = document.querySelectorAll('.container3 .card');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', bendera);
+
+function bendera() {
+    var reveals = document.querySelectorAll('.bendera');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+
+
+window.addEventListener('scroll', box);
+
+function box() {
+    var reveals = document.querySelectorAll('#box');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box2);
+
+function box2() {
+    var reveals = document.querySelectorAll('#box2');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box3);
+
+function box3() {
+    var reveals = document.querySelectorAll('#box3');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box4);
+
+function box4() {
+    var reveals = document.querySelectorAll('#box4');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box5);
+
+function box5() {
+    var reveals = document.querySelectorAll('#box5');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box6);
+
+function box6() {
+    var reveals = document.querySelectorAll('#box6');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box7);
+
+function box7() {
+    var reveals = document.querySelectorAll('#box7');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box8);
+
+function box8() {
+    var reveals = document.querySelectorAll('#box8');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
+window.addEventListener('scroll', box9);
+
+function box9() {
+    var reveals = document.querySelectorAll('#box9');
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 10;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('muncul');
+        } else {
+            reveals[i].classList.remove('muncul');
+        }
+    }
+};
